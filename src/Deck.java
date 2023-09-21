7dec6b520b67b487385482c2e2b79a68949d552c
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2023-09-21 19:01:21 -0300

    Shuffle Method

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,11 @@
+import java.security.SecureRandom;
+
+
 public class Deck {
     private Card[] deck; // array of cards
     private static final int DECK_SIZE = 52; // constant for the size of the deck
+
+    private static final SecureRandom rand = new SecureRandom(); // needed instatiates the class in order to use nextInt(); method
 
 
     Deck() {
@@ -16,6 +21,19 @@ public class Deck {
                 this.deck[cont] = new Card(faces[j], naipes[i]);
                 cont++;
             }
+        }
+        shuffleDeck();
+    }   
+
+    // shuffle deck method
+    public void shuffleDeck() {
+        int j = 0;
+        for (int i = 0; i < DECK_SIZE; i++) {
+            j = rand.nextInt(DECK_SIZE);
+
+            Card temp = this.deck[i];
+            this.deck[i] = this.deck[j];
+            this.deck[j] = temp;
         }
     }
 }
